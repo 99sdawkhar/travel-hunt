0316eba12ba84f6f945ec16a903cca1e311f8ff0
dshubhamprdxn <shubhamd@axioned.com>
2022-01-12 20:10:39 +0530

    feat: fetch places from gmaps

---
+++ src/api/index.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+
+export const getPlacesData = async ({ ne, sw }) => {
+  try {
+    const { data: { data } } = await axios.get(URL, {
+      params: {
+        bl_latitude: ne.lat,
+        tr_latitude: sw.lat,
+        bl_longitude: ne.lng,
+        tr_longitude: sw.lng,
+      },
+      headers: {
+        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+        'x-rapidapi-key': 'c2159313dbmshf0d27deaa94261ap1c89f6jsnee71dd8a6506'
+      }
+    });
+
+    return data;
+
+  } catch (err) {
+    console.log(err);
+  }
+}
