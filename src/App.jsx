8117469109c98595c30a5cb4af965773dd40e447
dshubhamprdxn <shubhamd@axioned.com>
2022-01-12 19:24:22 +0530

    feat: created basic structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Header from './components/Header/Header'
+import Map from './components/Map/Map';
+import List from './components/List/List';
+import Footer from './components/Footer/Footer';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Map />
+      <List />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
