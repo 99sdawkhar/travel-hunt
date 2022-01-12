138674666825230f3938dc30cdbf504ab65db021
dshubhamprdxn <shubhamd@axioned.com>
2022-01-12 19:56:57 +0530

    refactor: Header structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <AppBar>
+      <Toolbar>
+        <Typography variant="h5" >
+          Travel Hunt
+        </Typography>
+        <Box display="flex">
+          <Typography variant="h6" >
+            Explore new places
+          </Typography>
+          {/* <Autocomplete> */}
+            <div >
+              <div >
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search ... " />
+            </div>
+          {/* </Autocomplete> */}
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
