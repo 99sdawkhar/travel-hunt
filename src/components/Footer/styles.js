7bfef8618647384a9cd52dc1119f10bec0158265
dshubhamprdxn <shubhamd@axioned.com>
2022-01-15 20:16:32 +0530

    feat: added favicon

---
+++ src/components/Footer/styles.js
@@ -0,0 +1,11 @@
+import { makeStyles } from "@material-ui/styles";
+
+export default makeStyles((theme) => ({
+  footer: {
+    textAlign: 'center',
+    padding: '10px 0',
+    color: '#143050',
+    fontSize: '15px',
+    fontWeight: '600',
+  }
+}))
